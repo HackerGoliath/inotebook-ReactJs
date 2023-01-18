650b03afba2a23755aa391217d80135bbf6dc75f
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-01-18 15:04:16 +0530

    51 Creating Middleware

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
-const mongoURI = "mongodb://127.0.0.1:27017/mydb";
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook";
 // const mongoURI = "mongodb :localhost:27017/?readPreference=primary&appname=MongoDB%2ØCompass&directConnection=true&ssl=fa1se";
 
 const connectToMongo = () => {
